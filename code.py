5ddf71d9d51ddf84416b4459108ebc76b32a3e9e
cailujia <cailujia@htbaobao.com>
2020-01-19 17:10:59 +0800

    write a smiple demo

---
+++ code.py
@@ -0,0 +1,14 @@
+import  qrcode
+
+qr = qrcode.QRCode(
+    version= 2,
+    error_correction= qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 1
+)
+
+qr.add_data('https://m.baidu.com/?from=1012852s')
+qr.make(fit=True)
+img = qr.make_image()
+img.save('my_qrcode.png')
+
